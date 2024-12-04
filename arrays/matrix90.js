e256baaa6c2ff399d5ec41e7c4ebe959f7f6b93b
Harikrishna Sri Ramoji <sriramojiharikrishna7@gmail.com>
2024-12-04 19:00:47 +0530

    matrx 90

---
+++ arrays/matrix90.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+
+    let len = matrix.length
+
+    for(let  i = 0;i<len;i++){
+    for(let  j = i+1;j<len;j++){
+
+    [matrix[i][j],matrix[j][i]]= [matrix[j][i],matrix[i][j]]
+
+    }
+
+
+        
+
+    }
+
+        for(let  i = 0;i<len;i++){
+        matrix[i].reverse()
+    }
+    
+};
