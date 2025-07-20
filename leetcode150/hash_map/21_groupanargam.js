0fe819302c378fca70abe3462c8dccfe9756e2ad
Harikrishna Sri Ramoji <sriramojiharikrishna7@gmail.com>
2025-07-20 23:49:28 +0530

    groupanargam

---
+++ leetcode150/hash_map/21_groupanargam.js
@@ -0,0 +1,15 @@
+var groupAnagrams = function(strs) {
+    const map = new Map();
+
+    for (let word of strs) {
+        const key = word.split('').sort().join('');
+        
+        if (!map.has(key)) {
+            map.set(key, []);
+        }
+
+        map.get(key).push(word);
+    }
+
+    return Array.from(map.values());
+};
